b1c44c6821c0cad3785a29b1ad3f7a133b536e32
Naveen Kumar Medi <medinaveenkumar9@gmail.com>
2025-05-09 10:42:55 +0530

    added  default port

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 // Middleware
 app.use(cookieParser());
-const allowedOrigins = ['http://localhost', 'http://172.26.0.50'];
+const allowedOrigins = ['http://localhost', 'http://localhost:3000', 'http://172.26.0.50', 'http://172.26.0.50:3000' ];
 
 app.use(cors({
   origin: function (origin, callback) {
